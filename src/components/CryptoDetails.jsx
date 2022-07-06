ec4f110824b3848821684ab73a974102c172b9bf
ninatch <ninatch13@gmail.com>
2022-07-06 15:52:50 +0400

    basic layout

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CryptoDetails = () => {
+  return (
+    <h1>CryptoDetails</h1>
+  )
+}
+
+export default CryptoDetails
---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cryptocurrencies = () => {
+  return (
+    <h1>Cryptocurrencies</h1>
+  )
+}
+
+export default Cryptocurrencies
