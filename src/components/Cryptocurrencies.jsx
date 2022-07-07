d2654203fd0800d3327f5c66be6dd210c58e71e2
ninatch <ninatch13@gmail.com>
2022-07-07 17:55:13 +0400

    fixed search

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -5,7 +5,7 @@ import { Card, Row, Col, Input } from 'antd'
 
 import { useGetCryptosQuery } from '../services/cryptoApi'
 
-const Cryptocurrencies = () => {
+const Cryptocurrencies = (simplified) => {
   const count = simplified ? 10 : 100
   const { data: cryptosList, isFetching } = useGetCryptosQuery()
   const [cryptos, setCryptos] = useState([])
@@ -21,9 +21,11 @@ const Cryptocurrencies = () => {
 
   return (
     <>
-      <div className='search-crypto'>
-        <input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)}/>
-      </div>
+      {!simplified && (
+        <div className='search-crypto'>
+          <input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)}/>
+        </div>
+      )}
       <Row gutters={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
