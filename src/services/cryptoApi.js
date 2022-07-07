501a7e4aa13074c027186ffe07b4727970d360ce
ninatch <ninatch13@gmail.com>
2022-07-07 17:53:22 +0400

    added coin fetching functionality

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,21 +1,31 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
     'X-RapidAPI-Key': '2c78b74302msh3ace3655fa06f84p13546ejsn47a9d7c8dfc5',
+    //should this be 'coinranking60d06371434a8abc218a7d5a06a5174adc9bebbf5fb8aea9'?
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com/coins'
+//should this be 'https://api.coinranking.com/v2'?
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: () => '/exchanges'
+            query: () => createRequest('/coins')
         })
     })
 })
+
+export const {
+    useGetCryptosQuery
+} = cryptoApi
+
+
 // const options = {
 //   method: 'GET',
 //   url: 'https://coinranking1.p.rapidapi.com/coins',
